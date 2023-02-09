3d51d2f79e3b5bce3f1f5bcd0b4900e612bae2ae
SiraKim0 <dosira686@naver.com>
2023-02-10 06:46:58 +0900

    Set: ts, webpack 재설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,49 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const prod = process.env.NODE_ENV === "production";
-const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
+const webpack = require("webpack");
 
-module.exports = {
-  mode: prod ? "production" : "development",
-  devtool: prod ? "hidden-source-map" : "eval",
-  entry: "./src/index.tsx",
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: ["babel-loader", "ts-loader"],
-      },
+module.exports = (env, argv) => {
+  const prod = argv.mode === "production";
+
+  return {
+    mode: prod ? "production" : "development",
+    devtool: prod ? "hidden-source-map" : "eval",
+    entry: "./src/index.tsx",
+    output: {
+      path: path.join(__dirname, "/dist"),
+      filename: "[name].js",
+    },
+    devServer: {
+      port: 3000,
+      hot: true,
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: ["babel-loader", "ts-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        React: "react",
+      }),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        minify:
+          process.env.NODE_ENV === "production"
+            ? {
+                collapseWhitespace: true, // 빈칸 제거
+                removeComments: true, // 주석 제거
+              }
+            : false,
+      }),
+      new CleanWebpackPlugin(),
     ],
-  },
-  devServer: {
-    historyApiFallback: true,
-    inline: true,
-    port: 3000,
-    hot: true,
-    publicPath: "/",
-  },
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
-  },
-
-  plugins: [
-    new webpack.ProvidePlugin({
-      React: "react",
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
+  };
 };
