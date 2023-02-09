7fc09995f5054910cadb073393e48aaa8910c49c
SiraKim0 <dosira686@naver.com>
2023-02-10 02:17:12 +0900

    Set: babel, typescript 패키지 설정

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const prod = process.env.NODE_ENV === "production";
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  mode: prod ? "production" : "development",
+  devtool: prod ? "hidden-source-map" : "eval",
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["babel-loader", "ts-loader"],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    port: 3000,
+    hot: true,
+    publicPath: "/",
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
